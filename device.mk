0bb69ed72a6a33617477c1550f5eb77396f215e5
Vishalcj17 <vishalcj17@gmail.com>
2020-02-08 10:32:02 +0530

    mido:Build Bluetooh Packages
    Signed-off-by: Vishalcj17 <vishalcj17@gmail.com>
    Signed-off-by: Mangomanid <skgaming7869@gmail.com>

--- device.mk
+++ device.mk
@@ -116,6 +116,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
