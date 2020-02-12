3358969656975310283464118dcccab79e2c5a3f
Mangomanid <skgaming7869@gmail.com>
2020-02-12 13:37:28 +0530

    mido: Go Permissive :(

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
