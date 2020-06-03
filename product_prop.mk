a1205b0458459c9d845255011ec88ef55b152897
Nick C <ctwoon@suicide.today>
2020-06-04 00:33:57 +0300

    mido: props: Enable zygote preforking
    faster app launching
    
    Change-Id: I408566b74f0c241dfb858e0d1b9725f60dafe495

--- product_prop.mk
+++ product_prop.mk
@@ -8,3 +8,7 @@
 # Default to BFQ I/O scheduler
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.io.scheduler=bfq
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
